edd46c74c51d59c7d808d3d67e0b8d8fb8fdf8f4
xudi <xudi1989@ruc.edu.cn>
2016-10-19 11:26:45 +0800

    2016年 10月 19日 星期三 11:26:44 CST

--- cppLangTest/pretty_printer.cpp
+++ cppLangTest/pretty_printer.cpp
@@ -13,8 +13,9 @@ TEST(PRETTY_PRINT,LIBTABLE){
     tp.AddColumn("Age", 5);
     tp.AddColumn("Position", 30);
     tp.AddColumn("Allowance", 9);
-
+    tp.set_flush_left();
     tp.PrintHeader();
+
     tp << "Dat Chu" << 25 << "Research Assistant" << -0.00000000001337;
     tp << "John Doe" << 26 << "Too much float" << 125456789.123456789;
     tp << "John Doe" << 26 << "Typical Int" << 1254;
