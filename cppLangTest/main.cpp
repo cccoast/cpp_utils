2eed24d6d565294356cfc6500ed37bd9128fedaa
xudi <xudi1989@ruc.edu.cn>
2017-04-20 16:47:57 +0800

    2017年 04月 20日 星期四 16:47:57 CST

--- cppLangTest/main.cpp
+++ cppLangTest/main.cpp
@@ -5,10 +5,23 @@
 
 using namespace std;
 
-int main(){
+/*int main(){
 
     int argc = 2;
     char* argv[] = { "ALL_TEST","--gtest_filter=PRETTY_PRINT.*"};
     testing::InitGoogleTest(&argc,argv);
     return RUN_ALL_TESTS();
+}
+*/
+
+int main(){
+
+    int a = 1, b = 100;
+    int sum = 0;
+
+    for (int i = 0; i < b; i ++)
+        sum += i;
+
+    cout << sum << endl;
+    return 0;
 }
