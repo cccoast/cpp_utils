9342dbbbaa63596a547846daeea44d18951c9737
coast <xudi1989@ruc.edu.cn>
2019-01-12 23:03:04 +0800

    time queue lambda value capture solved

--- timer/main.cpp
+++ timer/main.cpp
@@ -1,6 +1,7 @@
 
 #include <memory>
 #include <iostream>
+#include <vector>
 #include "timer.h"
 #include "timerQueue.h"
 
@@ -18,10 +19,11 @@ void tvshift(struct timeval &tv, long milliSec) {
 }
 
 int main(){
-    boost::asio::io_service io;
-    timer mytimer(io);
+    //boost::asio::io_service io;
+    //timer mytimer(io);
+    timer mytimer;
     mytimer.start();
-    int expire = 5000;
+    int expire = 10000;
     struct timeval tv;
     gettimeofday(&tv, NULL);
     tvshift(tv, expire);
@@ -32,56 +34,17 @@ int main(){
         shared_ptr<int> count = make_shared<int>(10);
         wkp = count;
         print_use_count(wkp.use_count());
-        callBackFunction print_func = [count](){ cout << "callback = " << count.use_count() << ' ' << *count << endl; };
+        callBackFunction print_func = [count](){ cout << "left callback = " << count.use_count() << endl; };
         print_use_count(wkp.use_count());
-        cout << "hi" << endl;
-        mytimer.post(te,print_func);
-        cout << "hi" << endl;
-        mytimer.post(te,[count](){ cout << "callback = " << count.use_count() << ' ' << *count << endl; });
+        mytimer.post_timestamp(te,print_func);
+        mytimer.post_duration(10,0,[count](){ cout << "right callback = " << count.use_count() << endl; });
         print_use_count(wkp.use_count());
     }
     print_use_count(wkp.use_count());
     while(true){
-        sleep(4);
-        cout << wkp.use_count() << endl;
+        sleep(2);
+        print_use_count(wkp.use_count());
     }
     return 0;
 }
 
-/*
-class timerTest{
-public:
-	static int count;
-	void run(void* arg) {
-		cout << "count = " << count << ", pointer = " << *(int *)arg << endl;
-		count++;
-	}
-	void deconstruct(void* arg) {
-		cout << "deconstruct" << endl;
-		if (arg)
-			free(arg);
-	}
-};
-int timerTest::count = 0;
-
-int main(){
-    timerTest tt;
-    int expire = 3000;
-    int tid = 1;
-	boost::asio::io_service io;
-	timer timer_test(io, boost::bind(&timerTest::run, &tt, _1), boost::bind(&timerTest::deconstruct, &tt, _1));
-	timer_test.start();
-	struct timeval tv;
-    gettimeofday(&tv, NULL);
-    tvshift(tv, expire);
-    int* p = (int *)malloc(sizeof(int));
-    *p = tid;
-    timeEvent te(tv.tv_sec, tv.tv_usec);
-    te.arg = p;
-    timer_test.post(te);
-    while(true){
-        sleep(10);
-    }
-    delete p;
-}
-*/
--- timer/timer.h
+++ timer/timer.h
@@ -22,32 +22,31 @@ using callBackFunction = std::function<void(void)>;
 
 class timer :public boost::noncopyable
 {
+    unique_ptr<boost::asio::io_service> iop;
 	unique_ptr<boost::asio::io_service::work> idle;
-	unique_ptr<boost::asio::io_service> iop;
-	boost::asio::io_service& io;
 public:
-	boost::posix_time::time_duration getDurationFromNow(timeEvent& time_event) {
+	shared_ptr<boost::posix_time::time_duration> getDurationFromNow(timeEvent& time_event) {
 		struct timeval tv;
 		gettimeofday(&tv,NULL);
 		long milliSecFromNow = (time_event.first - tv.tv_sec) * 1000 + int((time_event.second - tv.tv_usec) / 1000);
 		milliSecFromNow = milliSecFromNow > 0 ? milliSecFromNow : 0;
-		boost::posix_time::time_duration tmp = boost::posix_time::time_duration(boost::posix_time::millisec(milliSecFromNow));
-		return std::move(tmp);
+		return make_shared<boost::posix_time::time_duration>(boost::posix_time::millisec(milliSecFromNow));
 	}
 
-	timer(boost::asio::io_service& _io):
-		io(_io),
-		idle(make_unique<boost::asio::io_service::work>(_io))
-	{}
-
-	timer() = delete;
+	timer():
+		iop(make_unique<boost::asio::io_service>()),
+        idle(make_unique<boost::asio::io_service::work>(*iop))
+	{
+	}
 
 	void start() {
-		std::thread t(boost::bind(&boost::asio::io_service::run, &io));
+		std::thread t(boost::bind(&boost::asio::io_service::run, iop.get()));
 		t.detach();
 	}
 
-	void timeOut(const boost::system::error_code& ec,std::decay<callBackFunction>::type& callback_func, shared_ptr<boost::asio::deadline_timer>& _dtimer) {
+	void timeOut(const boost::system::error_code& ec,
+                std::decay<callBackFunction>::type& callback_func,
+                shared_ptr<boost::asio::deadline_timer>& _dtimer) {
 		if(!ec)
 		{
             callback_func();
@@ -57,14 +56,27 @@ public:
 		}
 	}
 
-	void post(timeEvent& te, std::decay<callBackFunction>::type callback_func) {
-		auto duration = getDurationFromNow(te);
+    //std::decay<callBackFunction>::type
+	void post_timestamp(timeEvent& te, callBackFunction callback_func) {
+		auto duration{std::move(getDurationFromNow(te))};
 		auto task = [duration,callback_func,this]() {
-			shared_ptr<boost::asio::deadline_timer> pnewTimer(new boost::asio::deadline_timer(this->io,duration));
-			pnewTimer->async_wait(boost::bind(&timer::timeOut, this, boost::asio::placeholders::error,
-                                                callback_func, pnewTimer));
+			shared_ptr<boost::asio::deadline_timer> pnewTimer(new boost::asio::deadline_timer(*iop,*duration));
+			pnewTimer->async_wait(boost::bind(&timer::timeOut, this,
+                                              boost::asio::placeholders::error,
+                                              callback_func, pnewTimer));
 		};
-		this->io.post(task);
+		iop->post(task);
+	}
+
+	void post_duration(unsigned int seconds,unsigned int milli_seconds,callBackFunction callback_func){
+        auto duration{std::move(make_shared<boost::posix_time::time_duration>(boost::posix_time::millisec(seconds * 1000 + milli_seconds)))};
+        auto task = [duration,callback_func,this]() {
+			shared_ptr<boost::asio::deadline_timer> pnewTimer(new boost::asio::deadline_timer(*iop,*duration));
+			pnewTimer->async_wait(boost::bind(&timer::timeOut, this,
+                                              boost::asio::placeholders::error,
+                                              callback_func, pnewTimer));
+		};
+		iop->post(task);
 	}
 };
 
